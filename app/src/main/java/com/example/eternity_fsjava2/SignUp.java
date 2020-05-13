c2c781d304f339781c0c9b9a8471cb13cc17014c
Atul Pandey <atulcp2001@gmail.com>
2020-05-13 15:11:05 -0700

    Commit 3 - Cleaned up imports, nullable GetUid() call and always true authresult statement
    Signed-off-by: Atul Pandey <atulcp2001@gmail.com>

--- app/src/main/java/com/example/eternity_fsjava2/SignUp.java
+++ app/src/main/java/com/example/eternity_fsjava2/SignUp.java
@@ -1,35 +1,30 @@
 package com.example.eternity_fsjava2;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.io.ObjectStreamException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class SignUp extends AppCompatActivity {
-
-    private static final String TAG = "SignUp";
 
     private static final String KEY_NAME = "UserFullName";
     private static final String KEY_EMAIL = "UserEmail";
@@ -38,9 +33,8 @@ public class SignUp extends AppCompatActivity {
     private EditText mFullName, mEmail, mPassword, mPhone;
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
-    private TextView mSignInText;
     private ProgressBar mProgressBar;
-    private Button mSignUpButton;
+
 
 
     @Override
@@ -53,21 +47,8 @@ public class SignUp extends AppCompatActivity {
         mPassword = findViewById(R.id.password);
         mPhone = findViewById(R.id.phoneNumber);
 
-
-
-        mSignUpButton = findViewById(R.id.signUpButton);
-        mSignInText = findViewById(R.id.textSignIn);
         mProgressBar = findViewById(R.id.progressBar);
 
-        // When Sign In Text View is clicked
-
-        /*mSignInText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), LogIn.class));
-                finish();
-            }
-        });*/
 
     }
 
@@ -112,40 +93,39 @@ public class SignUp extends AppCompatActivity {
 
         //Create the user
 
+        mProgressBar.setVisibility(View.VISIBLE);
         mAuth.createUserWithEmailAndPassword(email,password)
                 .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
                     @Override
                     public void onSuccess(AuthResult authResult) {
 
-                        if (authResult.getUser().getUid() !=null){
+                        mProgressBar.setVisibility(View.GONE);
+                        String UserID = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
+                        Toast.makeText(SignUp.this, "User Created...", Toast.LENGTH_SHORT).show();
+                        Log.d("ACP", "User Created....");
 
-                            String UserID = mAuth.getCurrentUser().getUid();
-                            Toast.makeText(SignUp.this, "User Created...", Toast.LENGTH_SHORT).show();
-                            Log.d("ACP", "User Created....");
+                        Map<String, Object> profile = new HashMap<>();
 
-                            Map<String, Object> profile = new HashMap<>();
+                        profile.put(KEY_NAME, userFullName);
+                        profile.put(KEY_EMAIL, email);
+                        profile.put(KEY_PHONE, userPhoneNumber);
 
-                            profile.put(KEY_NAME, userFullName);
-                            profile.put(KEY_EMAIL, email);
-                            profile.put(KEY_PHONE, userPhoneNumber);
-
-                            mStore.collection("Users").document(UserID).set(profile)
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Toast.makeText(SignUp.this, "User Profile Saved...", Toast.LENGTH_SHORT).show();
-                                    Log.d("ACP", "User Profile Saved..."+ mStore.collection("Users").toString());
-                                }
-                            }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(SignUp.this, "Failure...."+e.toString(), Toast.LENGTH_SHORT).show();
-                                    Log.d("ACP", "Failure...."+e.toString());
-                                }
-                            });
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-
-                        }
+                        mStore.collection("Users").document(UserID).set(profile)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(SignUp.this, "User Profile Saved...", Toast.LENGTH_SHORT).show();
+                                Log.d("ACP", "User Profile Saved..."+ mStore.collection("Users").toString());
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(SignUp.this, "Failure...."+e.toString(), Toast.LENGTH_SHORT).show();
+                                Log.d("ACP", "Failure...."+e.toString());
+                            }
+                        });
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
 
                     }
                 })
