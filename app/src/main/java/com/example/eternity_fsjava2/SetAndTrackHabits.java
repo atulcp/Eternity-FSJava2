d938dcd319d0d188cfcba5bb3ea293d602a5280b
Atul Pandey <atulcp2001@gmail.com>
2020-06-03 09:37:42 -0700

    Commit 12 - After fixing the HabitCounter logic - 03Jun2020
    Signed-off-by: Atul Pandey <atulcp2001@gmail.com>

--- app/src/main/java/com/example/eternity_fsjava2/SetAndTrackHabits.java
+++ app/src/main/java/com/example/eternity_fsjava2/SetAndTrackHabits.java
@@ -16,10 +16,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
