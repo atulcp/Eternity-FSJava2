7d7b446bd936b29c6e823c380e0ff9faa149cef4
Atul Pandey <atulcp2001@gmail.com>
2020-06-26 05:50:09 -0700

    Commit 15 - Code Cleanup for Setting Habits, Logcat debugs suppressed - 26Jun2020
    Signed-off-by: Atul Pandey <atulcp2001@gmail.com>

--- app/src/main/java/com/example/eternity_fsjava2/SetAndTrackHabits.java
+++ app/src/main/java/com/example/eternity_fsjava2/SetAndTrackHabits.java
@@ -22,13 +22,11 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
 import java.util.Date;
@@ -41,7 +39,6 @@ public class SetAndTrackHabits extends AppCompatActivity {
     private static final String KEY_NAME = "UserFullName";
 
     private static final String KEY_HABIT_NAME = "HabitName ";
-   // private static final String KEY_HABIT_NUMBER = "HabitNumber ";
     private static final String KEY_HABIT_DETAIL = "HabitDetail ";
     private static final String KEY_HABIT_COUNT = "HabitNumberCount ";
     private static final String KEY_HABIT_CREATEDON = "HabitCreatedOn ";
@@ -67,7 +64,8 @@ public class SetAndTrackHabits extends AppCompatActivity {
         mFullName = findViewById(R.id.ProfileNameValue);
         mHabitName = findViewById(R.id.habitName);
 
-        Log.d("ACP", "Entering Set Habit Screen For...."+ Objects.requireNonNull(mAuth.getCurrentUser()).getEmail());
+        //26 Jun 2020 - suppressing the below logcat message originally created for debugging to check if the user logged in is the current user
+        //Log.d("ACP", "Entering Set Habit Screen For...."+ Objects.requireNonNull(mAuth.getCurrentUser()).getEmail());
 
         DocumentReference documentReference = mStore.collection("Users").document(UserID);
 
@@ -148,25 +146,16 @@ public class SetAndTrackHabits extends AppCompatActivity {
 
 
         // 03Jun2020: Log a message in the Logcat to check if the latest habitNum is 1 greater than the last HabitCounter count
-        Log.d("ACP", "The Habit Number is now: "+ habitNum);
+        // 26 Jun 2020 - Suppressed logcat message originally created for debugging
+        // Log.d("ACP", "The Habit Number is now: "+ habitNum);
 
         // 03Jun2020 - Added userHabitNum Map to capture the Habit Details for each habitNum in a separate Map:
         final Map<String, Object> userHabit = new HashMap<>();
-        //final Map<String, Object> userHabitNum = new HashMap<>();
         final Map<String, Object> userHabitDetail = new HashMap<>();
 
         //03Jun2020: Step 1- Update the userHabitDetail Map first
-        //userHabitDetail.put(KEY_HABIT_NAME + habitNum, userHabitName );
         userHabitDetail.put(KEY_HABIT_NAME , userHabitName );
         userHabitDetail.put(KEY_HABIT_CREATEDON, new Timestamp(new Date()) );
-
-        //03Jun2020: Step 2- Update the userHabitNum Map with userHabitDetail Map inside it next
-        //userHabitNum.put(KEY_HABIT_NUMBER, habitNum);
-        //userHabitNum.put(KEY_HABIT_DETAIL, userHabitDetail);
-
-//        userHabit.put(KEY_HABIT_COUNT, habitNum);
-//        userHabit.put(KEY_HABIT_NUMBER + habitNum, userHabitName);
-
 
         userHabit.put(KEY_HABIT_DETAIL + habitNum , userHabitDetail);
         userHabit.put(KEY_HABIT_COUNT, habitNum);
@@ -181,9 +170,8 @@ public class SetAndTrackHabits extends AppCompatActivity {
                         @Override
                         public void onSuccess(Void aVoid) {
                             Toast.makeText(SetAndTrackHabits.this, "User HABIT Saved...", Toast.LENGTH_SHORT).show();
-                            //Log.d("ACP", "User HABIT Saved for user..."+ userEmail + " Habit Name..." + userHabitDetail.get(KEY_HABIT_NAME + habitNum));
-                            //Log.d("ACP", "User HABIT Saved for user..."+ userEmail + " Habit Name..." + userHabit.get(KEY_HABIT_NAME + habitNum));
                             Log.d("ACP", "User HABIT Saved for user..."+ userEmail + " Habit Name..." + userHabitDetail.get(KEY_HABIT_NAME));
+                            startActivity(new Intent(getApplicationContext(), SetAndTrackHabits.class));
                             checkIfHabitExists(userHabitName); // 24 Jun 2020
                             checkAndUpdateHabitCounter();
                         }
@@ -205,21 +193,22 @@ public class SetAndTrackHabits extends AppCompatActivity {
         //03Jun2020 - Logic to find if the Habit document in the FireStore exists for the user.
         // If it doesn't exist then set the habitNum to 1.
         // If it exists, then get the latest HabitCounter from the document and set the habitNum to 1 more than the HabitCounter.
+        // 26 Jun 2020 - suppressing logcat messages created initially for debugging
 
         DocumentReference HabitDocRef = mStore.collection("Habits").document(UserID);
 
         HabitDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
-                Log.d("ACP", "Inside HabitDocRef....On Success...");
+                //Log.d("ACP", "Inside HabitDocRef....On Success...");
                 if(!documentSnapshot.exists()){
                     habitNum = 1;
-                    Log.d("ACP", "Inside HabitDocRef....If document doesn't exist..");
+                    //Log.d("ACP", "Inside HabitDocRef....If document doesn't exist..");
                     habitExists = false;
                 }else{
                     int latestCounter = Integer.parseInt(Objects.requireNonNull(Objects.requireNonNull(documentSnapshot.getData()).get(KEY_HABIT_COUNT)).toString());
                        habitNum = latestCounter + 1;
-                    Log.d("ACP", "Inside HabitDocRef....If document exists and is not null.." +habitNum);
+                    //Log.d("ACP", "Inside HabitDocRef....If document exists and is not null.." +habitNum);
                 }
             }
         });
@@ -234,90 +223,30 @@ public class SetAndTrackHabits extends AppCompatActivity {
 
     private void checkIfHabitExists(final String userHabitName){
 
-        //08 Jun 2020 - Check if the entered Habit Name doesn't already exist in the database
-
+        //25 Jun 2020 - Trying to find if the document already contains the Habit Name entered in the Set Habit screen
         DocumentReference documentReference = mStore.collection("Habits").document(UserID);
-        //CollectionReference colRef = mStore.collection("Habits");
-
-
-        //23 Jun 2020 - Trying to find if the document already contains the Habit Name entered in the Set Habit screen
 
         documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
 
+                //25 Jun2020 - adding toUpperCase() method to avoid duplication due to case
+                // 26 Jun 2020 - suppressing logcat messages created initially for debugging
+
                 if(task.isSuccessful()){
                     DocumentSnapshot document = task.getResult();
-                    Log.d("ACP", "Checking Exsting document data..." + document.getData().toString());
-                    if(document.exists() && document.getData().toString().contains(userHabitName)){
-//                        Log.d("ACP", "Checking Exsting document data..." + Objects.requireNonNull(document.getData()).toString());
-//                        if(document.getData().toString().contains(userHabitName)){
-//                            mHabitName.setError("Habit already exists in the database...");
-//                        }else {
-//                            habitExists = false;
-//                        }
+                    //Log.d("ACP", "Checking Exsting document data..." + document.getData().toString());
+                    String userHabitNameCaps = userHabitName.toUpperCase();
+                    if(document.exists() && document.getData().toString().toUpperCase().contains(userHabitNameCaps)){
                         habitExists = true;
                         mHabitName.setError("Habit already exists in the database...");
 
                     }else {
-                        Log.d("ACP", "Habit doesn't exist in the database....setting the habitExists flad to false..." );
+                        //Log.d("ACP", "Habit doesn't exist in the database....setting the habitExists flag to false..." );
                         habitExists = false;
-
                     }
                 }
             }
         });
-
-
-//        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//            @Override
-//            public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                Log.d("ACP", "Validating if the Habit already exists....in the database...");
-//                if(documentSnapshot.exists()){
-//                    if(documentSnapshot.getData().toString().contains(userHabitName)){
-//                        Log.d("ACP", "Query Snapshot is not null..." );
-//                        mHabitName.setError("Habit already exists in the database...");
-//                    }else{
-//                        habitExists = false;
-//                    }
-//                }else {
-//                    habitExists = false;
-//                }
-//
-//
-//            }
-//        });
-
-
-//        colRef.whereEqualTo(KEY_HABIT_NAME, userHabitName).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-//            @Override
-//            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-//
-//                if(queryDocumentSnapshots != null){
-//                    Log.d("ACP", "Query Snapshot is not null..." + queryDocumentSnapshots.toString());
-//                    mHabitName.setError("Habit already exists in the database...");
-//                }else {
-//                    habitExists = false;
-//                }
-//            }
-//        });
-
-        //        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//            @Override
-//            public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                Log.d("ACP", "Validating if the Habit already exists....in the database...");
-//                if(!documentSnapshot.exists()){
-//                    habitExists = false;
-//                }
-//                else
-//                    {
-//                    Log.d("ACP", "Document snapshot" + documentSnapshot.getData().toString());
-//                    String habitInDatabase = Objects.requireNonNull(Objects.requireNonNull(documentSnapshot.getData()).get(KEY_HABIT_NAME)).toString();
-//                    if(habitInDatabase.equals(userHabitName)){
-//                          mHabitName.setError("Habit already exists in the database...");
-//                    }
-//                }
-//            }
-//        });
     }
 }
